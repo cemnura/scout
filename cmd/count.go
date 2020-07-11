708fa2d43f9b303ea54fe877727b057f4dc37fb8
Cem Nura <cem.nura@gmail.com>
2020-07-11 17:45:55 +0300

    Temporary default path argument

--- cmd/count.go
+++ cmd/count.go
@@ -36,6 +36,12 @@ var countCmd = &cobra.Command{
 	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		ignoreCase, _ := rootCmd.PersistentFlags().GetBool("ignore-case")
+
+		//.TODO Find a way to give default values to arguments either then this
+		if len(args) == 1 {
+			args = []string{args[0], "."}
+		}
+
 		result, err := action.Count(args[0], args[1], ignoreCase)
 
 		if err != nil {
